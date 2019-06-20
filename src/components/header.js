2c4aad87c76182cdf93bcd228854552a3ad7c465
Tim Ryding <tryding2@gmail.com>
2019-06-20 15:31:25 -0600

    dropdown on mouseover for game button on headbar working

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button, Menu, MenuItem, WithStyles } from "@material-ui/core";
+import { withStyles } from "@material-ui/styles";
 
 /* This component will have the Astero name/logo on the top 
 of the header bar with links to different website sections below it, 
 and a larger link to the steam page below those */
 
-const Header = props => {
-    return (
-        <div>
-            <h1>ASTERO</h1>
-        </div>
-    )
-}
+const styles = {
+  button: {
+    margin: "5px"
+  }
+};
 
-export default Header
+const Header = props => {
+  const [open, setOpen] = useState(false);
+  const [anchor, setAnchor] = useState(null);
+  const { classes } = props;
+
+  const onHover = event => {
+    setAnchor(event.target);
+    setOpen(true);
+  };
+
+  return (
+    <div className="header">
+      <div>
+        <h1>ASTERO</h1>
+      </div>
+      <div className="header-buttons">
+        <Button containerElement={<Link to="/" />} variant="contained" className={classes.button}>
+          Home
+        </Button>
+        <Button containerElement={<Link to="/team" />} variant="contained" className={classes.button}>
+          Team
+        </Button>
+        <Button onMouseEnter={onHover} variant="contained" className={classes.button}>
+          Game
+        </Button>
+        <Menu
+          id="game-menu"
+          anchorEl={anchor}
+          open={open}
+          MenuListProps={{ onMouseLeave: () => setOpen(false) }}
+        >
+          <MenuItem containerElement={<Link to="/game/media" />}>
+            Media
+          </MenuItem>
+          <MenuItem containerElement={<Link to="/game/patches" />}>
+            Patch Notes
+          </MenuItem>
+          <MenuItem containerElement={<Link to="/game/roadmap" />}>
+            Roadmap
+          </MenuItem>
+        </Menu>
+        <Button containerElement={<Link to="/blog" />} variant="contained" className={classes.button}>
+          Blog
+        </Button>
+        <Button variant="contained" className={classes.button}>Forums</Button>
+      </div>
+      <div>
+        <Button variant="contained" className={classes.button}>Buy</Button>
+      </div>
+    </div>
+  );
+};
+
+export default withStyles(styles)(Header);
